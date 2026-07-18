e5cb5cdc91e58898d9c8d78265f4f798ca6e699b
RAMAN <ramanvermadtu@gmail.com>
2026-07-18 15:06:50 +0530

    Delete SQL directory

--- SQL/CREATE TABLES/Billing.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Billing (
-    BillingID INT PRIMARY KEY,
-    PatientID INT,
-    ServiceDate DATE,
-    ServiceType VARCHAR(100),
-    ServiceCost DECIMAL(18,2),
-    PaymentStatus VARCHAR(50), -- Paid / Pending
-    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID)
-);
--- SQL/CREATE TABLES/Departments.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Departments (
-    DepartmentID INT PRIMARY KEY,
-    HospitalID INT,
-    DepartmentName VARCHAR(100),
-    HeadOfDepartment VARCHAR(100),
-    FOREIGN KEY (HospitalID) REFERENCES Hospitals(HospitalID)
-);
